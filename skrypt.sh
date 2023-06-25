daad426b00b1aa57b15d2a89ff690b0a304902ca
makogra <pietrzak.maciek.pv@gmail.com>
2023-06-25 09:46:02 +0200

    zad2_gitignore

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,26 @@ if [ $1 == "--date" ]
 then
     echo $(date)
 fi
+
+#zad2
+create_logs() {
+    i=1 
+    while((i <= $1));do
+        file_name="log${i}.txt"
+        if [ -e $file_name ]
+        then
+            rm $file_name
+        fi
+        touch $file_name
+        echo $file_name >> $file_name
+        echo "$0" >> $file_name
+        date >> $file_name
+        let "i++"
+    done
+}
+
+if [ $1 == "--logs" ]
+then
+    create_logs 100
+fi
+
